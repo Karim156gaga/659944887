ca9471f5ecccc596ce7a662a37803b94b0114d77
Karim156gaga <158857297+Karim156gaga@users.noreply.github.com>
2024-02-04 23:33:24 +0100

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,11 +1,1 @@
-(function() {
-    'use strict';
 
-    // Fonction pour actualiser la page
-    function actualiserPage() {
-        location.reload();
-    }
-
-    // Actualiser la page toutes les 2 secondes
-    setInterval(actualiserPage, 1000);
-})();
