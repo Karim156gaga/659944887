049e14f618b2221972c59dc1e976e523ee9183de
Karim156gaga <158857297+Karim156gaga@users.noreply.github.com>
2024-02-04 23:37:47 +0100

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,1 +1,9 @@
+(function() {
+    'use strict';
 
+    // Affiche un message d'alerte
+    alert("Je peux tout faire!");
+
+    // Vous pouvez également utiliser console.log pour afficher un message dans la console du navigateur
+    console.log("Je peux tout faire! (affiché dans la console)");
+})();
